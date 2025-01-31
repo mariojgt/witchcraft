dfb9483c1b967f567c1fe4b83eeb88abdaeb4516
Mario Tarosso <mariojgt2@gmail.com>
2025-01-31 10:41:26 +0000

    updates

--- Publish/Resource/js/vue_components/editor.vue
+++ Publish/Resource/js/vue_components/editor.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { ref, onMounted } from 'vue';
-// import { WorkflowEditor } from 'witchcraft-vs/src/index.ts';
-// import 'witchcraft-vs/src/assets/main.css';
-import { WorkflowEditor } from '../../../../../repo/witchcraft-node/src/index.ts';
-import '../../../../../repo/witchcraft-node/src/assets/main.css';
+import { WorkflowEditor } from 'witchcraft-vs/src/index.ts';
+import 'witchcraft-vs/src/assets/main.css';
+// import { WorkflowEditor } from '../../../../../repo/witchcraft-node/src/index.ts';
+// import '../../../../../repo/witchcraft-node/src/assets/main.css';
 </script>
 
 <template>
